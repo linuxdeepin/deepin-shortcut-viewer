f5eddfc71773872747f5c0a515a39de3f3b8e16b
drgondjf <ding465398889@163.com>
2018-02-07 12:23:39 +0800

    Update: update licence
    Change-Id: Idd572566211747ebf6dd3a0e94101139913ad289

--- view/shortcutscene.h
+++ view/shortcutscene.h
@@ -1,4 +1,11 @@
 /*
+ * Copyright (C) 2016 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     dragondjf<dingjiangfeng@deepin.com>
+ *
+ * Maintainer: dragondjf<dingjiangfeng@deepin.com>
+ *             Tangtong<tangtong@deepin.com>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
